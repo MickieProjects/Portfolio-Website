01f2b2c7c23c6d5156e1637a5b496807f4456a5e
Khathathon Nissayan <143895455+MickieProjects@users.noreply.github.com>
2023-12-30 15:15:07 +0700

    setup project

---
+++ src/assets/data/data.js
@@ -0,0 +1,85 @@
+export const Bio = {
+  name: "Khathathon Nissayan",
+  nickname: "Mickie",
+  roles: ["Frontend Developer", "UI Designer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/MickieProjects",
+  resume:
+    "https://drive.google.com/file/d/11J1UUvzNL3zbApJAb7ZvjCHppv6fQBFD/view?usp=sharing",
+  youtube: "https://www.youtube.com/channel/UCKzpgGZsiU3skBGyRYlzQlw",
+  tiktok: "https://www.tiktok.com/@_mickiex",
+};
+
+export const skills = [
+  {
+    title: "Skills",
+    skills: [
+      {
+        name: "HTML5",
+        image: "../assets/images-skills/hmtl.png",
+      },
+      {
+        name: "CSS3",
+        image: "../assets/images-skills/css.png",
+      },
+      {
+        name: "JavaScript",
+        image: "../assets/images-skills/js.png",
+      },
+      {
+        name: "TailwindCSS",
+        image: "../assets/images-skills/tailwind.png",
+      },
+      {
+        name: "React",
+        image: "../assets/images-skills/react.png",
+      },
+      {
+        name: "Git",
+        image: "../assets/images-skills/git.png",
+      },
+      {
+        name: "Github",
+        image: "../assets/images-skills/github.png",
+      },
+      {
+        name: "DaisyUI",
+        image: "../assets/images-skills/daisyui.png",
+      },
+    ],
+  },
+  {
+    title: "Other Skills",
+    skills: [
+      {
+        name: "After Effect",
+        image: "../assets/images-skills/ae.png",
+      },
+      {
+        name: "After Effect",
+        image: "../assets/images-skills/ae.png",
+      },
+      {
+        name: "illustrator",
+        image: "../assets/images-skills/illustrator.png",
+      },
+      {
+        name: "Photoshop",
+        image: "../assets/images-skills/photoshop.png",
+      },
+      {
+        name: "Premium Pro",
+        image: "../assets/images-skills/pr.png",
+      },
+      {
+        name: "Figma",
+        image: "../assets/images-skills/figma.png",
+      },
+      {
+        name: "Blender",
+        image: "../assets/images-skills/blender.png",
+      },
+    ],
+  },
+];
